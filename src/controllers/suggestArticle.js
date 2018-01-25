d2b46ccaf9c8aaf91ce97c7e0fe6e01e53ae1d01
Alex <alexey.shulev@gmail.com>
2018-01-25 16:12:26 +0200

    first parse atemps

--- src/controllers/suggestArticle.js
+++ src/controllers/suggestArticle.js
@@ -1,17 +1,42 @@
 const axios = require('axios');
+const htmlparser = require('htmlparser2');
+const CircluarJSON = require('circular-json');
+
+const parseArticle = (html, res) => {
+	const handler = new htmlparser.DomHandler((error, dom) => {
+		if (error) {
+			console.log(error);
+			res.sendStatus(500);
+		} else {
+			res.json(CircluarJSON.stringify(dom)).status(200);
+		}
+	});
+
+	const parser = new htmlparser.Parser(handler);
+
+	parser.write(html);
+	parser.end();
+};
+
+
 
 function suggestArticle (req, res) {
 	const url = req.query.articleURL;
-	if (url) {
-		axios.get(url)
-			.then((res) => {
-				console.log(res)
-			})
-			.catch((err) => {
-				console.log(err);
-			})
+	if (!url) {
+		return res.sendStatus(404);
 	}
-	res.sendStatus(200);
+	axios({
+		method: 'get',
+		url,
+		'Content-type': 'text/html'
+	})
+		.then((response) => {
+			parseArticle(response.data, res);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.sendStatus(500);
+		})
 }
 
 module.exports = suggestArticle;
